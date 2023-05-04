12e64aa83f18e10b37e0fc17284ba3e3db67e39e
Le Ngoc Linh <lnlinh93@dinsight.ai>
2023-05-04 14:57:28 +0900

    [TCT-23] chores - rename all *.cpp file to *.cc according to google style guide - add inspector-client test

---
+++ gst-tofplugin/test/inspector/inspector-client.cc
@@ -0,0 +1,38 @@
+#include <gmock/gmock.h>
+#include <gst/gst.h>
+#include <gtest/gtest.h>
+#include <lib/inspector/inspector-client.h>
+
+#include <iostream>
+
+class InspectorClientMock : public InspectorClient {
+ public:
+  virtual ~InspectorClientMock(){};
+  MOCK_METHOD(void, Update, (GstBuffer*), (override));
+};
+
+class InspectorClientTest : public ::testing::Test {
+ protected:
+  void SetUp() override { test_client = new InspectorClientMock(); }
+
+  void TearDown() override { delete test_client; }
+
+  InspectorClientMock* test_client;
+};
+
+TEST_F(InspectorClientTest, TestSetCaps) {
+  char caps_str[128];
+  int frame_width = 640;
+  int frame_height = 480;
+  sprintf(caps_str, "video/tof,format=DA_F32,width=%d,height=%d", frame_width,
+          frame_height);
+  GstCaps* caps = gst_caps_from_string(caps_str);
+  test_client->SetCaps(caps);
+  gst_caps_unref(caps);
+
+  int width, height;
+  test_client->GetFrameSize(width, height);
+
+  EXPECT_EQ(width, frame_width);
+  EXPECT_EQ(height, frame_height);
+}
