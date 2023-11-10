cd0d29598dd90457e089b24a403536d9a737035a
Le Ngoc Linh <lnlinh93@dinsight.ai>
2023-11-10 11:38:07 +0900

    [TCT-31] GraphicsScene continue 🐛 fix(graphics-layout.cc): fix layout functions to skip hidden children when calculating layout 🐛 fix(graphics-layout.h): add default margins and size to layout constructors ✨ feat(graphics-layout.cc): add printLayout function to print the layout info for debugging ✨ feat(graphics-layout.h): add support for GraphicsGridLayout to create a grid layout with specified number of rows and columns ✅ test(graphics-layout.cc): add test for GraphicsGridLayout

--- apps/tct-gui/graphics-layout.cc
+++ apps/tct-gui/graphics-layout.cc
@@ -1,18 +1,84 @@
 #include "graphics-layout.h"
 
-GraphicsLayout::GraphicsLayout(const std::string& name)
-    : GraphicsItem(name), margins_(0, 0, 0, 0), preferredSize_(0, 0) {}
+#include <functional>
+#include <iostream>
+#include <sstream>
+
+GraphicsLayout::GraphicsLayout(const std::string& name, ImVec2 preferredSize,
+                               ImVec4 margins)
+    : GraphicsItem(name), margins_(margins), preferredSize_(preferredSize) {}
+
 void GraphicsLayout::layout() {}
 
-GraphicsHBoxLayout::GraphicsHBoxLayout(const std::string& name)
-    : GraphicsLayout(name) {}
+std::string GraphicsLayout::printLayout() {
+  std::function<void(GraphicsLayout*, int, std::ostringstream&)>
+      printLayoutRecursive = [&](GraphicsLayout* layout, int depth,
+                                 std::ostringstream& layoutStr) {
+        std::string leading;
+        GraphicsLayout* current = layout;
+        GraphicsLayout* parent =
+            dynamic_cast<GraphicsLayout*>(current->parent_);
+        bool isLast = true;
+        if (parent != nullptr) {
+          isLast = (current == parent->children_.back().get());
+        }
+        leading = (isLast) ? "  └►[" : "  ├►[";
+        while (parent != nullptr) {
+          GraphicsLayout* grandparent =
+              dynamic_cast<GraphicsLayout*>(parent->parent_);
+          bool parentIsLast = true;
+          if (grandparent != nullptr) {
+            parentIsLast = (parent == grandparent->children_.back().get());
+          }
+          if (parentIsLast) {
+            leading = "   " + leading;
+          } else {
+            leading = "  |" + leading;
+          }
+          parent = grandparent;
+        }
+
+        std::string type;
+        if (dynamic_cast<GraphicsHBoxLayout*>(layout) != nullptr) {
+          type = "HBox";
+        } else if (dynamic_cast<GraphicsVBoxLayout*>(layout) != nullptr) {
+          type = "VBox";
+        } else if (dynamic_cast<GraphicsGridLayout*>(layout) != nullptr) {
+          type = "Grid";
+        } else {
+          type = "Layout";
+        }
+
+        layoutStr << leading << type << "]: " << layout->name_ << " pos: ("
+                  << layout->pos_.x << "," << layout->pos_.y
+                  << "), preferredSize: (" << layout->preferredSize_.x << ","
+                  << layout->preferredSize_.y << ")" << std::endl;
+
+        for (auto childit = layout->children_.begin();
+             childit != layout->children_.end(); ++childit) {
+          auto childLayout = dynamic_cast<GraphicsLayout*>(childit->get());
+          if (childLayout != nullptr) {
+            printLayoutRecursive(childLayout, depth + 1, layoutStr);
+          }
+        }
+      };
+
+  std::ostringstream layoutStr;
+  printLayoutRecursive(this, 0, layoutStr);
+
+  return layoutStr.str();
+}
+
+GraphicsHBoxLayout::GraphicsHBoxLayout(const std::string& name,
+                                       ImVec2 preferredSize, ImVec4 margins)
+    : GraphicsLayout(name, preferredSize, margins) {}
 
 void GraphicsHBoxLayout::layout() {
   ImVec2 pos(margins_.x, margins_.y);
   float maxChildHeight = 0;
   for (auto child : children_) {
     auto childLayout = dynamic_cast<GraphicsLayout*>(child.get());
-    if (childLayout != nullptr) {
+    if (childLayout != nullptr && !childLayout->isHidden_) {
       // layout the children inside first
       childLayout->layout();
       childLayout->setPos(pos);
@@ -30,15 +96,16 @@ void GraphicsHBoxLayout::layout() {
   }
 }
 
-GraphicsVBoxLayout::GraphicsVBoxLayout(const std::string& name)
-    : GraphicsLayout(name) {}
+GraphicsVBoxLayout::GraphicsVBoxLayout(const std::string& name,
+                                       ImVec2 preferredSize, ImVec4 margins)
+    : GraphicsLayout(name, preferredSize, margins) {}
 
 void GraphicsVBoxLayout::layout() {
   ImVec2 pos(margins_.x, margins_.y);
   float maxChildWidth = 0;
   for (auto child : children_) {
     auto childLayout = dynamic_cast<GraphicsLayout*>(child.get());
-    if (childLayout != nullptr) {
+    if (childLayout != nullptr && !childLayout->isHidden_) {
       // layout the children inside first
       childLayout->layout();
       childLayout->setPos(pos);
@@ -52,5 +119,69 @@ void GraphicsVBoxLayout::layout() {
   if (preferredSize_.y == 0) {
     preferredSize_.x = pos.x + maxChildWidth + margins_.z;
     preferredSize_.y = pos.y + margins_.w;
+  }
+}
+
+GraphicsGridLayout::GraphicsGridLayout(int rows, int cols,
+                                       const std::string& name,
+                                       ImVec2 preferredSize, ImVec4 margins)
+    : GraphicsLayout(name, preferredSize, margins), rows_(rows), cols_(cols) {}
+
+void GraphicsGridLayout::layout() {
+  float maxChildWidth[cols_];
+  float maxChildHeight[rows_];
+  // reset
+  for (int col = 0; col < cols_; col++) {
+    maxChildWidth[col] = 0;
+  }
+  for (int row = 0; row < rows_; row++) {
+    maxChildHeight[row] = 0;
+  }
+  // layout the children first
+  for (int row = 0; row < rows_; row++) {
+    for (int col = 0; col < cols_; col++) {
+      int index = row * cols_ + col;
+      if (index >= children_.size()) {
+        break;
+      }
+      auto child = children_[index].get();
+      auto childLayout = dynamic_cast<GraphicsLayout*>(child);
+      if (childLayout != nullptr && !childLayout->isHidden_) {
+        childLayout->layout();
+        if (childLayout->preferredSize_.y > maxChildHeight[row]) {
+          maxChildHeight[row] = childLayout->preferredSize_.y;
+        }
+        if (childLayout->preferredSize_.x > maxChildWidth[col]) {
+          maxChildWidth[col] = childLayout->preferredSize_.x;
+        }
+      }
+    }
+  }
+  // now position the children
+  ImVec2 rowPos(margins_.x, margins_.y);
+  ImVec2 colPos;
+  for (int row = 0; row < rows_; row++) {
+    colPos = rowPos;
+    for (int col = 0; col < cols_; col++) {
+      int index = row * cols_ + col;
+      if (index >= children_.size()) {
+        break;
+      }
+      auto child = children_[index].get();
+      auto childLayout = dynamic_cast<GraphicsLayout*>(child);
+      if (childLayout != nullptr && !childLayout->isHidden_) {
+        childLayout->setPos(colPos);
+        childLayout->clip(ImRect(colPos, colPos + childLayout->preferredSize_));
+        colPos = colPos + ImVec2(maxChildWidth[col], 0.0f);
+      }
+    }
+    rowPos = rowPos + ImVec2(0.0f, maxChildHeight[row]);
+  }
+
+  if (preferredSize_.x == 0) {
+    preferredSize_.x = colPos.x + margins_.z;
+  }
+  if (preferredSize_.y == 0) {
+    preferredSize_.y = rowPos.y + margins_.w;
   }
 }
--- apps/tct-gui/graphics-layout.h
+++ apps/tct-gui/graphics-layout.h
@@ -7,10 +7,15 @@
  * set margin and preferred size to control the layout.
  * if preferred size is not set, it will be calculated from the children.
  * size = margin + children's size
+ *
+ * TODO: add support for stretch
  */
 struct GraphicsLayout : public GraphicsItem {
-  GraphicsLayout(const std::string& name = "");
+  GraphicsLayout(const std::string& name = "",
+                 ImVec2 preferredSize = ImVec2(0, 0),
+                 ImVec4 margins = ImVec4(0, 0, 0, 0));
   virtual void layout();
+  std::string printLayout();
   // left, top, right, bottom
   ImVec4 margins_;
   // width, height
@@ -18,11 +23,30 @@ struct GraphicsLayout : public GraphicsItem {
 };
 
 struct GraphicsHBoxLayout : public GraphicsLayout {
-  GraphicsHBoxLayout(const std::string& name = "");
+  GraphicsHBoxLayout(const std::string& name = "",
+                     ImVec2 preferredSize = ImVec2(0, 0),
+                     ImVec4 margins = ImVec4(0, 0, 0, 0));
   void layout() override;
 };
 
 struct GraphicsVBoxLayout : public GraphicsLayout {
-  GraphicsVBoxLayout(const std::string& name = "");
+  GraphicsVBoxLayout(const std::string& name = "",
+                     ImVec2 preferredSize = ImVec2(0, 0),
+                     ImVec4 margins = ImVec4(0, 0, 0, 0));
   void layout() override;
+};
+
+/**
+ * @brief child layout are added in row major order, aka left to right, top to
+ * bottom. If you add more than rows * cols children, the extra child layouts
+ * will be ignored, automatically position at top left of the grid.
+ *
+ */
+struct GraphicsGridLayout : public GraphicsLayout {
+  GraphicsGridLayout(int rows, int cols, const std::string& name = "",
+                     ImVec2 preferredSize = ImVec2(0, 0),
+                     ImVec4 margins = ImVec4(0, 0, 0, 0));
+  void layout() override;
+  int rows_;
+  int cols_;
 };
